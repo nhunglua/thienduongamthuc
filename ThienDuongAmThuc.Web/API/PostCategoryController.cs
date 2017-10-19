6e9e355821a603448d272d2c0dc703db1da439be
DESKTOP-1R9I958\Nhung-PC <lenhungth9x@gmail.com>
2017-10-19 16:14:45 +0700

    add dependency injection autofac to web

--- ThienDuongAmThuc.Web/API/PostCategoryController.cs
+++ ThienDuongAmThuc.Web/API/PostCategoryController.cs
@@ -1,22 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ThienDuongAmThuc.Model.Models;
 using ThienDuongAmThuc.Service;
 using ThienDuongAmThuc.Web.Infrastructure.Core;
+
 namespace ThienDuongAmThuc.Web.API
 {
+    [RoutePrefix("api/postcategory")]
     public class PostCategoryController : ApiControllerBase
     {
-        IPostCategoryService _postCategoryService;
-        public PostCategoryController(IErrorService errorService , IPostCategoryService postCategoryService) : base(errorService)
+        private IPostCategoryService _postCategoryService;
+
+        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) : base(errorService)
         {
             this._postCategoryService = postCategoryService;
         }
+
+        [Route("getall")]
         public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var listcategory = _postCategoryService.GetAll();
+
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listcategory);
+
+                return response;
+            });
+        }
+        [Route("Post")]
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -27,25 +41,7 @@ namespace ThienDuongAmThuc.Web.API
                 }
                 else
                 {
-                    var listcategory = _postCategoryService.GetAll();
-              
-                    response = request.CreateResponse(HttpStatusCode.OK, listcategory);
-                }
-                return response;
-            });
-        }
-
-        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
-        {
-            return CreateHttpResponse(request, () =>
-            {
-                HttpResponseMessage response = null;
-                if (ModelState.IsValid) {
-                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                }
-                else
-                {
-                   var category= _postCategoryService.Add(postCategory);
+                    var category = _postCategoryService.Add(postCategory);
                     _postCategoryService.Save();
 
                     response = request.CreateResponse(HttpStatusCode.Created, category);
@@ -53,7 +49,7 @@ namespace ThienDuongAmThuc.Web.API
                 return response;
             });
         }
-
+        [Route("put")]
         public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
         {
             return CreateHttpResponse(request, () =>
@@ -73,8 +69,8 @@ namespace ThienDuongAmThuc.Web.API
                 return response;
             });
         }
-
-        public HttpResponseMessage Delete(HttpRequestMessage request,int id)
+        [Route("delete")]
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -94,4 +90,4 @@ namespace ThienDuongAmThuc.Web.API
             });
         }
     }
-}
+    }
---
+++ ThienDuongAmThuc.Web/App_Start/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using ThienDuongAmThuc.Data.Infrastructure;
+using ThienDuongAmThuc.Data;
+using ThienDuongAmThuc.Data.Repositories;
+using ThienDuongAmThuc.Service;
+using System.Web.Mvc;
+using System.Web.Http;
+using Autofac.Integration.WebApi;
+
+[assembly: OwinStartup(typeof(ThienDuongAmThuc.Web.App_Start.Startup))]
+
+namespace ThienDuongAmThuc.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutofac(app);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+        public void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            //Register your WebAPI controllers'
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<ThienDuongAmThucDbContext>().AsSelf().InstancePerRequest();
+
+            //Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerRequest();
+
+            // Service
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerRequest();
+
+            //gans tats ca nhung thu o tren vao thung chua.
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); // ca api va ca controller co the su dung chung.
+        }
+    }
+}
