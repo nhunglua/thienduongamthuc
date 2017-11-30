5c8bc87ff3238527c932893bf9680079c4796446
DESKTOP-1R9I958\Nhung-PC <lenhungth9x@gmail.com>
2017-11-30 18:15:26 +0700

    angular

---
+++ ThienDuongAmThuc.Web/Scripts/spa/app.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myApp = angular.module("myModule", []);
+
+myApp.controller("schoolController", schoolController);
+myApp.service('validatorService', validatorService);
+myApp.directive('onlineShopDirective', onlineShopDirective);
+
+schoolController.$inject = ['$scope', 'validatorService'];
+
+// declare
+function schoolController($scope, validatorService) {
+    $scope.checkNumber = function () {
+        $scope.message = validatorService.checkNumber(2);
+    }
+    $scope.num = 1;
+}
+
+function validatorService($window) {
+    return {
+        checkNumber:checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else
+        {
+            return  'This is old';
+        }
+    }
+}
+function onlineShopDirective() {
+    return {
+        restrict: "A",
+        templateUrl: "/Scripts/spa/onlineShopDirective.html"
+    }
+}
