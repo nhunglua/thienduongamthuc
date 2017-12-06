1129019b73866d8769fef1fb310f556658df3ead
DESKTOP-1R9I958\Nhung-PC <lenhungth9x@gmail.com>
2017-12-06 15:10:31 +0700

    add fitter status and format data in list

---
+++ ThienDuongAmThuc.Web/app/Shared/Filters/statusFilter.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="E:\CongNgheWebNC\Project\Git\ThienDuongAmThuc.Web\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true) {
+                return 'Kích hoạt';
+            }
+            else
+            {
+                return 'Khóa';
+            }
+        }
+    });
+})(angular.module('onlineShop.common'));
